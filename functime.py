083f1cb5fb61c8f5422abb321cab31bdce026a24
Sheenam <45314018+Sheenam3@users.noreply.github.com>
2022-01-10 11:33:36 +0800

    added detach probes, signal handler

--- functime.py
+++ functime.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python
 #Usage: To get user function time by attaching uprobe and uretprobe to instrument function entry and exit time.
-#sheenam@sheenam:/usr/share/bcc/tools$ sudo ./functime.py -s /proc/20856/root/usr/share/bcc/tools/if-else -fn goNum
+#sheenam@sheenam:/usr/share/bcc/tools$ sudo ./functime.py -i 1 -s /proc/20856/root/usr/share/bcc/tools/if-else -fn goNum
 #TIME      PID    LATms           FNAME
 #22:03:57  20856  0.014978           goNum 
 #22:03:58  20856  0.007614           goNum 
 #22:03:59  20856  0.008696           goNum 
 #22:04:00  20856  0.013339           goNum 
-
 from __future__ import print_function
 from bcc import BPF
-from time import strftime
 import argparse
-
+from time import sleep, strftime
+from signal import signal, SIGTERM
 examples = """examples:
     ./functime           # time functime calls
     ./functime -p 181    # only trace PID 181
@@ -26,11 +25,16 @@ parser.add_argument("-s", "--shared", nargs="?",
         help="specify file path")
 parser.add_argument("-fn", "--fn", nargs="?",
         help="specify function name")
-parser.add_argument("--ebpf", action="store_true",
-    help=argparse.SUPPRESS)
+parser.add_argument("-i", "--interval",
+        help="summary interval, seconds")
 args = parser.parse_args()
 name = args.shared if args.shared else "not valid file path"
 fname = args.fn if args.fn else "Please enter the function name"
+if not args.interval:
+            args.interval = 99999999
+matched = 0
+trace_functions = {}
+t = 1
 # load BPF program
 bpf_text = """
 #include <uapi/linux/ptrace.h>
@@ -47,6 +51,7 @@ struct data_t {
     char comm[TASK_COMM_LEN];
 };
 BPF_HASH(start, u32, struct val_t);
+BPF_HASH(ts, u32, struct data_t);
 BPF_PERF_OUTPUT(events);
 int do_entry(struct pt_regs *ctx) {
     if (!PT_REGS_PARM1(ctx))
@@ -61,9 +66,9 @@ int do_entry(struct pt_regs *ctx) {
         start.update(&pid, &val);
     }
     return 0;
-}
+ }
 int do_return(struct pt_regs *ctx) {
-    struct val_t *valp;
+struct val_t *valp;
     struct data_t data = {};
     u64 delta;
     u32 pid = bpf_get_current_pid_tgid();
@@ -74,32 +79,39 @@ int do_return(struct pt_regs *ctx) {
     bpf_probe_read(&data.comm, sizeof(data.comm), valp->comm);
     data.pid = valp->pid;
     data.delta = tsp - valp->ts;
+    ts.update(&pid,&data);
     events.perf_submit(ctx, &data, sizeof(data));
     start.delete(&pid);
     return 0;
-}
+   }
 """
-if args.ebpf:
-    print(bpf_text)
-    exit()
-
+trace_functions[matched] = fname
 b = BPF(text=bpf_text)
 b.attach_uprobe(name=name, sym=fname, fn_name="do_entry", pid=args.pid)
 b.attach_uretprobe(name=name, sym=fname, fn_name="do_return",
                    pid=args.pid)
 
-# header
 print("%-9s %-6s %s %18s" % ("TIME", "PID", "LATms", "FNAME"))
 
-def print_event(cpu, data, size):
-    event = b["events"].event(data)
-    print("%-9s %-6d %s %15s " % (strftime("%H:%M:%S"), event.pid,
-         (float(event.delta) / 1000000), fname))
+def handler(signal_received, frame):
+    # Handle any cleanup here
+    b.detach_uprobe(name=name, sym=fname)
+    b.detach_uretprobe(name=name, sym=fname)
+    exit()
 
-# loop with callback to print_event
-b["events"].open_perf_buffer(print_event)
-while 1:
+signal(SIGTERM, handler)
+exiting = 0
+seconds = 0
+while t:
     try:
-        b.perf_buffer_poll()
+        sleep(int(args.interval))
+        seconds += int(args.interval)
+        ts = b["ts"]
+        for key, val in sorted(ts.items()):
+                print("%-9s %-6d %s %15s" % (strftime("%H:%M:%S"), val.pid,
+                (float(val.delta) / 1000000),fname))
     except KeyboardInterrupt:
+        exiting = 1
+    if exiting:
+        print("Detaching...")
         exit()
